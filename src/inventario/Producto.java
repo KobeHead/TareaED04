84cbdd3fd80ed541529960ecb6ee082d78e44bee
Diego <diegoserranotoca@gmail.com>
2025-02-06 18:40:10 +0100

    Versión con los atributos encapsulados

--- src/inventario/Producto.java
+++ src/inventario/Producto.java
@@ -1,9 +1,9 @@
 package inventario;
 
 public class Producto {
-    public String n;
-    public double p;
-    public int s;
+    private String n;
+    private double p;
+    private int s;
 
     public Producto(String nombre, double precio, int stock) {
         this.n = nombre;
@@ -12,12 +12,36 @@ public class Producto {
     }
 
     public void mostrarInformacion() {
-        System.out.println("Producto: " + n + " | Precio: " + p + " | Stock: " + s);
+        System.out.println("Producto: " + getN() + " | Precio: " + getP() + " | Stock: " + getS());
     }
 
     public void borrarDatos() {
-        n = "";
-        p = 0.0;
-        s = 0;
+        setN("");
+        setP(0.0);
+        setS(0);
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    public double getP() {
+        return p;
+    }
+
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public void setS(int s) {
+        this.s = s;
     }
 }
